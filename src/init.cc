3f5f53c10d4eff2c74551f790ce77bfeecb3aaed
bengfarrell <ben@benfarrell.com>
2014-12-02 22:53:18 -0800

    Added background subtractor init

--- src/init.cc
+++ src/init.cc
@@ -8,6 +8,7 @@
 #include "CamShift.h"
 #include "HighGUI.h"
 #include "FaceRecognizer.h"
+#include "BackgroundSubtractor.h"
 #include "Constants.h"
 
 
@@ -20,8 +21,9 @@ init(Handle<Object> target) {
     Matrix::Init(target);
     CascadeClassifierWrap::Init(target);
     VideoCaptureWrap::Init(target);
+    BackgroundSubtractorWrap::Init(target);
     Contour::Init(target);
-	  TrackedObject::Init(target);
+	TrackedObject::Init(target);
     NamedWindow::Init(target);
     Constants::Init(target);
 
